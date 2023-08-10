4bf2b07971da9bc5ac23f88b07dbac6b29d877d3
victoromuya <viczik16@gmail.com>
2023-08-10 09:07:41 -0700

    file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,16 +5,15 @@
 #include <unistd.h>
 
 /**
- * main - Returns  "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - Returns  "and that piece of art is useful"
  *
  * Return: Always 1.
  */
 int main(void)
 {
 write(2,
-	       	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	       	59);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 
 return (1);
 }
