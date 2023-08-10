a2f73ac28239bb5e2b20aedcfd4670812f6da7e7
victoromuya <viczik16@gmail.com>
2023-08-10 08:49:38 -0700

    file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -18,6 +17,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 	return (0);
 }
