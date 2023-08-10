d8ed411ef0930967ed3c773f748311b38fc070a9
victoromuya <viczik16@gmail.com>
2023-08-10 08:18:55 -0700

    file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 
+/*
+ * File: 4-puts.c
+ */
+
 #include <stdio.h>
 
+/*
+ * A program that has a main function and uses 
+ * the puts  function to return the text
+ */
+
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle \n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
 }
