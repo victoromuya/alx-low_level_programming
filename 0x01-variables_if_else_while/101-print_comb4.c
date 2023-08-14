dd0e198f3f9a173b73ea4c91669fc638b8867c2f
victoromuya <viczik16@gmail.com>
2023-08-14 00:38:11 -0700

    file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,19 @@
  */
 int main(void)
 {
-
-	for (int digit1 = 0; digit1 < 8; digit1++)
+	int num1,  num2, num3;
+	
+	for (num1 = 0; num1 < 8; num1++)
 	{
-		for (int digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (int digit3 = digit2 + 1; digit3 < 10; digit3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
