5d0ec1d840e064a191d429b87cf80403fe7753ef
victoromuya <viczik16@gmail.com>
2023-08-14 00:10:01 -0700

    file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,6 +13,7 @@
 int main(void)
 {
 	int num;
+	
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -21,6 +22,7 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
 
 	return (0);
