34d6bc2bb511c8295b652e89c054565e85c9486c
victoromuya <viczik16@gmail.com>
2023-08-13 23:30:23 -0700

    file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+/*
+ * File: 4-print_alphabt.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Returns the alphabet in lowercase, except for q and e.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' && letter == 'q')
+			continue;
+		putchar(letter)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
