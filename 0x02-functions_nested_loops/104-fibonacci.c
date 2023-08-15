5e17889046b394cd37a49588b6df690c75a7a7a7
victoromuya <viczik16@gmail.com>
2023-08-15 11:02:35 -0700

    file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,3 +1,10 @@
+/*
+ * File: 104-fibonacci.c
+ */
+
+#include <stdio.h>
+
+
 /**
  * main - Returns the first 98 Fibonacci numbers, starting with
  *        1 and 2, separated by a comma followed by a space.
