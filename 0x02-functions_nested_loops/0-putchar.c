cfafcccc106b72ae546ea51f6155ee47ce47749a
victoromuya <viczik16@gmail.com>
2023-08-15 06:53:35 -0700

    file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-putchar.c
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n')
+
+	return (0);
+}
+		
