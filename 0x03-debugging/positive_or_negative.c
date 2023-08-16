2b4f0ef0168e142e8485a9e0fcd7825f2b1597b9
victoromuya <viczik16@gmail.com>
2023-08-16 02:18:21 -0700

    file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,14 +1,21 @@
-/*File : positive_negative.c
- *Author : Victor Omuya
+/*
+ * File : positive_negative.c
+ * Author : Victor Omuya
  */
 
 #include "main.h"
 
-void positive_negative(int i)
+/*
+ * positive_or_negative - returns sign of the number.
+ *
+ * Return : what is in the print statement.
+ */
+
+void positive_or_negative(int i)
 {
-	if(i > 0)
+	if (i > 0)
 		printf("%d is positive\n", i);
-	else if(i < 0)
+	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
