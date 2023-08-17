bcd6a3080245b7cbf057b75b74548a346bf4c723
victoromuya <viczik16@gmail.com>
2023-08-17 07:02:17 -0700

    file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,8 +5,9 @@
  * File: main.h
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x04-more_functions_nested_loops directory.
- */
+*/
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
