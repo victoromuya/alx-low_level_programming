9b64867fbc4a944ee5da552ed5353f56e0872e5a
victoromuya <viczik16@gmail.com>
2023-09-21 07:43:41 -0700

    new com

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,14 @@
-/*
- * File: 100-first.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
+void first(void) __attribute__ ((constructor));
 
 /**
- * hare - Prints a string before the
- *        main function is executed.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void hare(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,10 @@
-; File: 101-hello_holberton.asm
-; Auth: Brennan D Baraban
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
-
-extern printf
-
-section .text
-   global main
-
+global   main
+	  extern    printf
 main:
-   push rbp
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0
