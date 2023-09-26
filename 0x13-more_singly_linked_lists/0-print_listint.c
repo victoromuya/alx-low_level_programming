225650b5dde1c52c8b0a49f99204aaf378025571
victoromuya <viczik16@gmail.com>
2023-09-26 09:01:55 -0700

    file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t thenodes = 0;
 
-	while(h)
+	while (h)
 	{
-		nodes++;
+		thenodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-
-	return (nodes);
+	return (thenodes);
 }
